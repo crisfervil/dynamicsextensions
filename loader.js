2390b44e66f184e0050def22ef891b157e63bca9
Cristhian Fernandez (C) <cfernand@water.ie>
2018-02-23 12:54:50 +0000

    added feature to window.open

--- loader.js
+++ loader.js
@@ -1,2 +1,2 @@
 var mainWindowUrl = 'https://crisfervil.github.io/dynamicsextensions/main.html';
-window.open(mainWindowUrl, 'width=300,height=300,alwaysRaised=yes,modal=yes');
+window.open(mainWindowUrl, 'width=300,height=300,alwaysRaised=yes,modal=yes,toolbar=no');
